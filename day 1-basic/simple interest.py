0fec06729bfe091b5e07bd9fd64730fbc98b6e82
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-06-25 11:09:26 +0530

    upload day 1-basic

---
+++ day 1-basic/simple interest.py
@@ -0,0 +1,6 @@
+p=int(input("enter the principle"))
+n=int(input("enter the number of year"))
+r=int(input("enter the rate of interest"))
+interest=p*n*r/100
+print(f"Interest amount to be paid {interest}")
+
