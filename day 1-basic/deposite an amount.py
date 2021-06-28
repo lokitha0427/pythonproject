1541174d88716ec25a42bb41c0a0f68775821e24
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-06-28 10:23:37 +0530

    Add files via upload

---
+++ day 1-basic/deposite an amount.py
@@ -0,0 +1,4 @@
+avl=int(input("enter the available balance"))
+dep=int(input("enter the deposit amount"))
+avl+=dep
+print(f"current available balance{avl}")
---
+++ day 1-basic/otp number.py
@@ -0,0 +1,2 @@
+n=int(input("enter the OTP"))
+print(n>=1000 and n<=9999)
