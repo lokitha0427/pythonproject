849de34873248bac72f873e54a8e2804815a31cc
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-06-29 11:17:19 +0530

    Delete area of perimeter.py

--- day 2 - operators/area of perimeter.py
+++
@@ -1,6 +0,0 @@
-l=int(input("enter the length value"))
-w=int(input("enter the width value"))
-area=l*w
-perimeter=2*(l+w)
-print(f"area of rectangle {area}")
-print(f"perimeter of rectangle {perimeter}")
