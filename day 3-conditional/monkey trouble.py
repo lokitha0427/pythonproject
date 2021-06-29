5abd8aa5cbf01217adb0c118d2e17d5bbe2e8def
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-06-29 17:09:48 +0530

    conditional

---
+++ day 3-conditional/monkey trouble.py
@@ -0,0 +1,18 @@
+#monkey trouble
+a_smile=False
+b_smile=False
+if ((a_smile and b_smile) or (not a_smile and not b_smile)):
+   print("true")
+else:
+   print("false")
+
+
+
+'''
+a_smile=bool(input("enter the a monkey"))
+b_smile=bool(input("enter the b monkey"))
+if True:
+   print(((a_smile and b_smile) or (not a_smile and not b_smile)))
+else:
+   print("false")
+'''
