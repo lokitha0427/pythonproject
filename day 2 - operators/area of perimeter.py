5626adaedd7a7e9a01ebb3175f5e46145898f32c
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-06-29 11:19:06 +0530

    Add files via upload

---
+++ day 2 - operators/area of perimeter.py
@@ -0,0 +1,6 @@
+l=int(input("enter the length value"))
+w=int(input("enter the width value"))
+area=l*w
+perimeter=2*(l+w)
+print(f"area of rectangle {area}")
+print(f"perimeter of rectangle {perimeter}")
