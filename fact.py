d3e7d152736cb550c90c8bd2b41d6ad4cda166e5
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-07-01 19:41:58 +0530

    for loop

---
+++ fact.py
@@ -0,0 +1,9 @@
+n=int(input("enter"))
+fact=1
+for i in range(1,n+1):
+        fact=fact*i
+        if i>1 and i<=n:
+                print(f"*{i}",end="")
+        else:
+           print(f"{i}",end="")
+print(f"={fact}")
