425103aa3830c25cabe57e4e0bcdc8cc30720a48
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-07-27 21:32:50 +0530

    types of function

---
+++ day 13-Functions/no return without argument.py
@@ -0,0 +1,7 @@
+#no return without argument
+def add():
+    a=int(input("Enter the value of A:"))
+    b=int(input("Enter the value of B:"))
+    c=a+b
+    print("Total:",c)
+add()
---
+++ day 13-Functions/return without argument.py
@@ -0,0 +1,7 @@
+#return without arugument
+def mul():
+    a=int(input("Enter the value of A:"))
+    b=int(input("Enter the value of B:"))
+    c=a*b
+    return c
+print(mul())
