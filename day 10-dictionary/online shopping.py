df4950d84b35698898508138b6ec60257ffc3fd7
lokitha0427 <86357064+lokitha0427@users.noreply.github.com>
2021-08-11 13:00:05 +0530

    Update online shopping.py

--- day 10-dictionary/online shopping.py
+++ day 10-dictionary/online shopping.py
@@ -1,5 +1,5 @@
 #online shopping
-product={101:["kitkat",10,5],102:["Dm,20,50"],103:["Milky bar",30,20],104:["Perk",40,30],105:["Munch,50,30"]}
+product={101:["kitkat",10,5],102:["Dm",20,50],103:["Milky bar",30,20],104:["Perk",40,30],105:["Munch",50,30]}
 sum=0
 while True:
     id=int(input("Enter the product ID:"))
